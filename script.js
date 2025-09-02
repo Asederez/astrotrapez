0f6ec71e26ad2c33bba063d7519cb721f8ddf89b
Asederez <k.wiacek01@gmail.com>
2025-09-02 09:16:01 +0200

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-function showSection(id) { const sections = document.querySelectorAll("main section"); sections.forEach(section => section.classList.remove("active"));
-
-const selected = document.getElementById(id); if (selected) { selected.classList.add("active"); } }
-
